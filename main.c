a44b88f272d8e97d06c28a7b77c53e4d8fcb4f89
Rumit2121 <nurgaleevtimur04@gmail.com>
2025-01-06 00:15:45 +0300

    С-DO-3.feature: Добавить вывод информации о массивах и их обход
    Этот коммит добавляет новую функциональность для
    -Вывода начального адреса каждого массива
    -Вывода размера всего массива и размера его отдельного элемента
    -Вывода дельты между адресами первого и последнего элементов массива
    -Реализация циклов обхода элементов массивов с использованием макроса
    для вычисления кол-ва элементов

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#define arrayLength(arr) (sizeof(arr)/sizeof(arr[0]))
 
 int main(){
 	
+	//----- 1 -----
 	// Создание массивов и статическая инициализация их элементов в форматах ДВШВЭ
 	char charAr[8] = {8, 10, -07, -11, 25, 0x1, 02, 14};
 	short shortAr[8] = {11, 55, 077, 0x10, -34, 41, 033, 12};
@@ -27,9 +29,9 @@ int main(){
 		printf("Dec: %d; Oct: %o; Hex: %#x\n", intAr[i], intAr[i], intAr[i]);
 	}
 
-	printf("\nlongAr:\n");
+	printf("\nlongArray:\n");
 	for(int i = 0; i < 8; i++){
-		printf("Dec: %d; Oct: %o; Hex: %#x\n", longAr[i], longAr[i], longAr[i]);
+		printf("Dec: %ld; Oct: %lo; Hex: %#lx\n", longAr[i], longAr[i], longAr[i]);
 	}
 
 	printf("\nfloatArray:\n");
@@ -46,4 +48,87 @@ int main(){
 	for(int i = 0; i < 8; i++){
 		printf("Dec: %Lf; Exp: %Le; Hex: %#La\n", longDoubleAr[i], longDoubleAr[i], longDoubleAr[i]);
 	}
+
+	//----- 2 -----
+	//Вывод начального адреса каждого массива, размер всего массива и размер отдельного элемента массива
+	printf("charArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", charAr, sizeof(charAr), sizeof(charAr[0]));
+	
+	printf("\nshortArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", shortAr, sizeof(shortAr), sizeof(shortAr[0]));
+
+	printf("\nintArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", intAr, sizeof(intAr), sizeof(intAr[0]));
+
+	printf("\nlongArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", longAr, sizeof(longAr), sizeof(longAr[0]));
+
+	printf("\nfloatArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", floatAr, sizeof(floatAr), sizeof(floatAr[0]));
+
+	printf("\ndoubleArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", doubleAr, sizeof(doubleAr), sizeof(doubleAr[0]));
+	
+	printf("\nlongDoubleArray:\n");
+	printf("Adress: %p; SizeAr: %zu; Size: %zu\n", longDoubleAr, sizeof(longDoubleAr), sizeof(longDoubleAr[0]));
+
+	// Вывод дельты между адресами первого и последнего элемента массива
+	printf("\ncharArray:\n");
+	printf("Delta: %td", (charAr + arrayLength(charAr) - 1) - charAr);
+	
+	printf("\nshortArray:\n");
+	printf("Delta: %td", (shortAr + arrayLength(shortAr) - 1) - shortAr);
+
+	printf("\nintArray:\n");
+	printf("Delta: %td", (intAr + arrayLength(intAr) - 1) - intAr);
+
+	printf("\nlongArray:\n");
+	printf("Delta: %td", (longAr + arrayLength(longAr) - 1) - longAr);
+
+	printf("\nfloatArray:\n");
+	printf("Delta: %td", (floatAr + arrayLength(floatAr) - 1) - floatAr);
+
+	printf("\ndoubleArray:\n");
+	printf("Delta: %td", (doubleAr + arrayLength(doubleAr) - 1) - doubleAr);
+	
+	printf("\nlongDoubleArray:\n");
+	printf("Delta: %td", (longDoubleAr + arrayLength(longDoubleAr) - 1) - longDoubleAr);
+
+	//Реализация циклов обхода элементов массивов на основе типового расчёта количества элементов в массиве в виде макровыражения
+	printf("\ncharArray:\n");
+	for(int i = 0; i < arrayLength(charAr); i++){
+		printf("charAr[%d] = %d\t", i, charAr[i]);
+	}
+	
+	printf("\nshortArray:\n");
+	for(int i = 0; i < arrayLength(shortAr); i++){
+		printf("shortAr[%d] = %d\t", i, shortAr[i]);
+	}
+
+	printf("\nintArray:\n");
+	for(int i = 0; i < arrayLength(intAr); i++){
+		printf("intAr[%d] = %d\t", i, intAr[i]);
+	}
+
+	printf("\nlongArray:\n");
+	for(int i = 0; i < arrayLength(longAr); i++){
+		printf("longAr[%d] = %ld\t", i, longAr[i]);
+	}
+
+	printf("\nfloatArray:\n");
+	for(int i = 0; i < arrayLength(floatAr); i++){
+		printf("floatAr[%d] = %f\t", i, floatAr[i]);
+	}
+
+	printf("\ndoubleArray:\n");
+	for(int i = 0; i < arrayLength(doubleAr); i++){
+		printf("doubleAr[%d] = %lf\t", i, doubleAr[i]);
+	}
+	
+	printf("\nlongDoubleArray:\n");
+	for(int i = 0; i < arrayLength(longDoubleAr); i++){
+		printf("longDoubleAr[%d] = %Lf\t", i, longDoubleAr[i]);
+	}
+	
 }
+
