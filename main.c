4f10b13405041d4f79834fbf0c94058ae8455ccf
Rumit2121 <nurgaleevtimur04@gmail.com>
2025-01-05 15:56:45 +0300

    C-DO-2.chore: Вывести переменные в различных форматах
    -Создание по две переменных (min, max) для каждой модификации типа данных
    -Присваивание значений в 16-м формате
    -Вывод переменных в различных форматах

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdbool.h>
+#include<limits.h>
+#include<float.h>
 
 int main(){
 	// Создание переменных для каждого типа данных
@@ -48,5 +50,51 @@ int main(){
 	printf("Размер sLongVar: %zu\n", sizeof(sLongVar));
 	printf("Размер unLongVar: %zu\n", sizeof(unLongVar));
 
-	printf("Размер longDoubleVar: %zu\n", sizeof(longDoubleVar));	
+	printf("Размер longDoubleVar: %zu\n", sizeof(longDoubleVar));
+
+	/*
+	Создание по две переменных (min, max) для каждой модифакации типа данных
+	и присваивание соответствующих значений в 16-м формате
+	*/
+	signed char sCharMin = CHAR_MIN;
+	signed char sCharMax = CHAR_MAX;
+	unsigned char uCharMin = 0x00;
+	unsigned char uCharMax = UCHAR_MAX;
+	
+	signed int sIntMin = INT_MIN;
+	signed int sIntMax = INT_MAX;
+	unsigned int uIntMin = 0x00;
+	unsigned int uIntMax = UINT_MAX;
+
+	signed short sShortMin = SHRT_MIN;
+	signed short sShortMax = SHRT_MAX;
+	unsigned short uShortMin = 0x00;
+	unsigned short uShortMax = USHRT_MAX;
+
+	signed long sLongMin = LONG_MIN;
+	signed long sLongMax = LONG_MAX;
+	unsigned long uLongMin = 0X00;
+	unsigned long uLongMax = ULONG_MAX;
+
+	long double doubleMin = DBL_MIN;
+	long double doubleMax = DBL_MAX;
+
+	//Вывод значения пееременных в десятичном, шестнадцатеричном, вещественном и экспоненциальном форматах
+	printf("\nВывод значений целых типов\n");
+	printf("(10)signed char: Min = %d; Max = %d\nunsigned char: Min = %u; Max = %u\n", sCharMin, sCharMax, uCharMin, uCharMax);
+	printf("(16)signed char: Min = %x; Max = %x\nunsigned char: Min = %#x; Max = %#x\n\n", sCharMin, sCharMax, uCharMin, uCharMax);
+	
+	printf("(10)signed int: Min = %d; Max = %d\nunsigned int: Min = %u; Max = %u\n", sIntMin, sIntMax, uIntMin, uIntMax);
+	printf("(16)signed int: Min = %x; Max = %x\nunsigned int: Min = %#x; Max = %#x\n\n", sIntMin, sIntMax, uIntMin, uIntMax);
+	
+	printf("(10)signed short: Min = %d; Max = %d\nunsigned short: Min = %u; Max = %u\n", sShortMin, sShortMax, uShortMin, uShortMax);
+	printf("(16)signed short: Min = %x; Max = %x\nunsigned short: Min = %#x; Max = %#x\n\n", sShortMin, sShortMax, uShortMin, uShortMax);
+	
+	printf("(10)signed long: Min = %ld; Max = %ld\nunsigned long: Min = %lu; Max = %lu\n", sLongMin, sLongMax, uLongMin, uLongMax);
+	printf("(16)signed long: Min = %lx; Max = %lx\nunsigned long: Min = %#lx; Max = %#lx\n\n", sLongMin, sLongMax, uLongMin, uLongMax);
+	
+	printf("\nВывод значений long double\n");
+	printf("(10, вещ)long double: Min = %Lf; Max = %Lf\n", doubleMin, doubleMax);
+	printf("(10, эксп)long double: Min = %Le; Max = %Le\n", doubleMin, doubleMax);
+	printf("(16)long double: Min = %La; Max = %La\n", doubleMin, doubleMax);
 }
