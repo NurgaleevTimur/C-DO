b5bf10aa083d42b0697cff0c345e5df14e700674
Rumit2121 <nurgaleevtimur04@gmail.com>
2025-01-13 03:38:21 +0300

    C-DO-4.feature: Реализовать UNION

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int main(){
 
 	//------ 1 ------
+	printf("------ 1 ------\n");
 	//Объявление структуры из 9 полей
 	struct s_truct
 	{
@@ -18,21 +19,56 @@ int main(){
 	};
 
 	//Определение переменной структуры и её статическая инициализация
-	struct s_truct first_var = {'a', 1, 10, 15, 3.14f, 2.7156, 1.23456, 1000000, 255};
+	struct s_truct struct_var = {'a', 1, 10, 15, 3.14f, 2.7156, 1.23456, 1000000, 255};
 
 	//Вывод размера структуры, размера переменной, адрес переменной, адрес каждого поля переменной, значение каждого поля переменной
 	printf("Размер структуры: %zu байт\n", sizeof(struct s_truct));
-	printf("Размер переменной: %zu байт\n", sizeof(first_var));
-	printf("Адрес переменной: %p\n", (void*)&first_var);
+	printf("Размер переменной: %zu байт\n", sizeof(struct_var));
+	printf("Адрес переменной: %p\n", (void*)&struct_var);
 	
-	printf("Адрес char_field: %p, значение: %c\n", (void*)&first_var.char_field, first_var.char_field);
-	printf("Адрес short_field: %p, значение: %d\n", (void*)&first_var.short_field, first_var.short_field);
-	printf("Адрес int_field: %p, значение: %d\n", (void*)&first_var.int_field, first_var.int_field);
-	printf("Адрес long_field: %p, значение: %ld\n", (void*)&first_var.long_field, first_var.long_field);
-	printf("Адрес float_field: %p, значение: %f\n", (void*)&first_var.float_field, first_var.float_field);
-	printf("Адрес double_field: %p, значение: %lf\n", (void*)&first_var.double_field, first_var.double_field);
-	printf("Адрес long_double_field: %p, значение: %Lf\n", (void*)&first_var.long_double_field, first_var.long_double_field);
-	printf("Адрес u_int_field: %p, значение: %u\n", (void*)&first_var.u_int_field, first_var.u_int_field);
-	printf("Адрес u_char_field: %p, значение: %u\n", (void*)&first_var.u_char_field, first_var.u_char_field);
+	printf("Адрес char: %p, значение: %c\n", (void*)&struct_var.char_field, struct_var.char_field);
+	printf("Адрес short: %p, значение: %d\n", (void*)&struct_var.short_field, struct_var.short_field);
+	printf("Адрес int: %p, значение: %d\n", (void*)&struct_var.int_field, struct_var.int_field);
+	printf("Адрес long: %p, значение: %ld\n", (void*)&struct_var.long_field, struct_var.long_field);
+	printf("Адрес float: %p, значение: %f\n", (void*)&struct_var.float_field, struct_var.float_field);
+	printf("Адрес double: %p, значение: %lf\n", (void*)&struct_var.double_field, struct_var.double_field);
+	printf("Адрес long: %p, значение: %Lf\n", (void*)&struct_var.long_double_field, struct_var.long_double_field);
+	printf("Адрес u_int: %p, значение: %u\n", (void*)&struct_var.u_int_field, struct_var.u_int_field);
+	printf("Адрес u_char: %p, значение: %u\n", (void*)&struct_var.u_char_field, struct_var.u_char_field);
+
+	//------ 2 ------
+	printf("\n------ 2 ------");
+	//Объявление структуры из 9 полей
+	union u_nion
+	{
+		char char_union;
+		short short_union;
+		int int_union;
+		long long_union;
+		float float_union;
+		double double_union;
+		long double long_double_union;
+		unsigned int u_int_union;
+		unsigned char u_char_union;
+	};
+
+	//Определение переменной объединения и её статическая инициализация
+	union u_nion union_var;
+	union_var.char_union = 55;
+
+	//Вывод размера объединения, размера переменной, адрес переменной, адрес каждого поля переменной, значение каждого поля переменной
+	printf("\nРазмер объединения: %zu\n", sizeof(union u_nion));
+	printf("Размер переменной: %zu\n", sizeof(union_var));
+	printf("Адрес переменной: %p\n", (void*)&union_var);
+
+	printf("Адрес char: %p, значение: %d\n", (void*)&union_var.char_union, union_var.char_union);
+	printf("Адрес short: %p, значение: %d\n", (void*)&union_var.short_union, union_var.short_union); 
+	printf("Адрес int: %p, значение: %d\n", (void*)&union_var.int_union, union_var.int_union);
+	printf("Адрес long: %p, значение: %ld\n", (void*)&union_var.long_union, union_var.long_union);
+	printf("Адрес float: %p, значение: %f\n", (void*)&union_var.float_union, union_var.float_union);
+	printf("Адрес double: %p, значение: %lf\n", (void*)&union_var.double_union, union_var.double_union);
+	printf("Адрес long: %p, значение: %Lf\n", (void*)&union_var.long_double_union, union_var.long_double_union);
+	printf("Адрес u_int: %p, значение: %u\n", (void*)&union_var.u_int_union, union_var.u_int_union);
+	printf("Адрес u_char: %p, значение: %u\n", (void*)&union_var.u_char_union, union_var.u_char_union);
 
 }
