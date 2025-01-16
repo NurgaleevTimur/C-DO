650c3da131fc09efb768e672098278d35f2deca2
Rumit2121 <nurgaleevtimur04@gmail.com>
2025-01-16 19:00:41 +0300

    C-DO-5.chore: Добавить прототип функции

--- main.c
+++ main.c
@@ -13,72 +13,8 @@ typedef enum{
 	U_CHAR_TYPE
 } DataType;
 
-// Функция для вывода элементов массива любого типа
-void print_array_elements(void* arr, size_t len, DataType type){
-	switch (type){
-		case CHAR_TYPE:{
-			char* char_arr = (char*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %d\n", i, &char_arr[i], char_arr[i]);
-			}
-			break;
-		}
-		case SHORT_TYPE:{
-			short* short_arr = (short*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %d\n", i, &short_arr[i], short_arr[i]);
-			}
-			break;
-		}
-		case INT_TYPE:{
-			int* int_arr = (int*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %d\n", i, &int_arr[i], int_arr[i]);
-			}
-			break;
-		}
-		case LONG_TYPE:{
-			long* long_arr = (long*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %ld\n", i, &long_arr[i], long_arr[i]);
-			}
-			break;
-		}
-		case FLOAT_TYPE:{
-			float* float_arr = (float*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %f\n", i, &float_arr[i], float_arr[i]);
-			}
-			break;
-		}
-		case DOUBLE_TYPE:{
-			double* double_arr = (double*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %lf\n", i, &double_arr[i], double_arr[i]);
-			}
-			break;
-		}
-		case U_INT_TYPE:{
-			unsigned int* u_int_arr = (unsigned int*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %u\n", i, &u_int_arr[i], u_int_arr[i]);
-			}
-			break;
-		}
-		case U_CHAR_TYPE:{
-			unsigned char* u_char_arr = (unsigned char*)arr;
-			for(size_t i = 0; i < len; i++){
-				printf("Адрес элемента №%zu: %p, значение: %u\n", i, &u_char_arr[i], u_char_arr[i]);
-			}
-			break;
-		}
-		
-		default:
-			break;
-	}
-}
-
-
+//Создание прототипа функции
+void print_array_elements(void*, size_t, DataType);
 
 int main(){
 
@@ -210,4 +146,71 @@ int main(){
 	printf("\nmas_u_char_union: \n");
     print_array_elements(ar_var.mas_u_char_union, sizeof(union u_nion_mas) / sizeof(ar_var.mas_u_char_union[0]), U_CHAR_TYPE);
 
+}
+
+
+
+// Функция для вывода элементов массива любого типа
+void print_array_elements(void* arr, size_t len, DataType type){
+	switch (type){
+		case CHAR_TYPE:{
+			char* char_arr = (char*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %d\n", i, &char_arr[i], char_arr[i]);
+			}
+			break;
+		}
+		case SHORT_TYPE:{
+			short* short_arr = (short*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %d\n", i, &short_arr[i], short_arr[i]);
+			}
+			break;
+		}
+		case INT_TYPE:{
+			int* int_arr = (int*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %d\n", i, &int_arr[i], int_arr[i]);
+			}
+			break;
+		}
+		case LONG_TYPE:{
+			long* long_arr = (long*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %ld\n", i, &long_arr[i], long_arr[i]);
+			}
+			break;
+		}
+		case FLOAT_TYPE:{
+			float* float_arr = (float*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %f\n", i, &float_arr[i], float_arr[i]);
+			}
+			break;
+		}
+		case DOUBLE_TYPE:{
+			double* double_arr = (double*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %lf\n", i, &double_arr[i], double_arr[i]);
+			}
+			break;
+		}
+		case U_INT_TYPE:{
+			unsigned int* u_int_arr = (unsigned int*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %u\n", i, &u_int_arr[i], u_int_arr[i]);
+			}
+			break;
+		}
+		case U_CHAR_TYPE:{
+			unsigned char* u_char_arr = (unsigned char*)arr;
+			for(size_t i = 0; i < len; i++){
+				printf("Адрес элемента №%zu: %p, значение: %u\n", i, &u_char_arr[i], u_char_arr[i]);
+			}
+			break;
+		}
+		
+		default:
+			break;
+	}
 }
