4c8bbac803147467a596e3bccc38a847939ab479
Nurgaleev Timur <nurgaleevtimur04@gmail.com>
2025-03-19 11:48:05 +0300

    C-DO-11.chore: Разработать функции добавления, удаления узлов и обход списка

--- main.c
+++ main.c
@@ -1,26 +1,29 @@
 #include "include/c.h"
 #include <stdio.h>
 
-
 int main ( void ){
-	struct __node1__ mas [ ] = {
-		{ mas + 1, 1 },
-		{ mas + 2, 2 },
-		{ mas + 3, 3 },
-		{ mas + 4, 4 },
-		{ mas + 5, 5 },
-		{ mas + 6, 6 },
-		{ mas + 7, 7 },
-		{ NULL	 , 8 }
 
-	};
+	struct __node1__ mas [ 8 ];
+
+	struct __node1__ node1 = { NULL, 1 };
+	struct __node1__ node2 = { NULL, 2 };
+	struct __node1__ node3 = { NULL, 3 };
+	struct __node1__ node4 = { NULL, 4 };
 	
-	struct __node1__ *node = mas;
 
-	while ( node != NULL ){
-		printf ( "%d\t", node->val );
-		node = node->next;
-	}
-  
-	printf ( "\n" );
+	__add1 ( mas, &node1 );
+	__add1 ( &node1, &node2 );
+	__add1 ( &node1, &node3 );
+	__add1 ( &node3, &node4 );
+
+	//1 3 4 2
+	__print1 ( &node1 );
+
+	//Должен удалиться 4 узел, то есть node2
+	__del1 ( &node4 );
+
+	//1 3 4
+	__print1 ( &node1 );
+
 }
+
